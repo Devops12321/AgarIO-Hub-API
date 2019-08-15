28e1559f64e0f755a164b2ca99accb4959007cc0
JJimenez_15 <34722775+JJimenez15@users.noreply.github.com>
2019-08-15 11:02:58 -0600

    Fix api.js

--- api.js
+++ api.js
@@ -3,21 +3,19 @@ const request = require('request')
 
 const BASE_URL = "http://api.agariohub.io/"
 
-function createData(json) {
-	return encodeURIComponent(btoa(JSON.stringify(json)).split("/").join("%"))
-}
-
-function connectLogin(page, jsonSend, callback, debug) {
-	var dataSend = createData(jsonSend)
-	if (!dataSend) {
-        console.error("Failed building data when trying to connect to: " + page)
-        return false
-    }
-	request(`${BASE_URL}${page}/${dataSend}`, (err, req, data) => {
-		var responseParse = JSON.parse(data)
-		if (debug) {
-			console.log(`[DEBUGGER] ${data}`)
+module.exports = {
+	connectLogin: function (page, jsonSend, callback, debug) {
+		var dataSend = encodeURIComponent(btoa(JSON.stringify(jsonSend)).split("/").join("%"))
+		if (!dataSend) {
+			console.error("Failed building data when trying to connect to: " + page)
+			return false
 		}
-		callback(responseParse)
-	});
-}
+		request(`${BASE_URL}${page}/${dataSend}`, (err, req, data) => {
+			var responseParse = JSON.parse(data)
+			if (debug) {
+				console.log(`[DEBUGGER] ${data}`)
+			}
+			callback(responseParse)
+		});
+	}
+};
