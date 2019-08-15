f6699fa8a7ba3b5cf2d4999683175d2b499583c8
JJimenez_15 <34722775+JJimenez15@users.noreply.github.com>
2019-08-15 12:09:44 -0600

    Update api.js

--- api.js
+++ api.js
@@ -7,8 +7,27 @@ const request = require('request')
 const BASE_URL = "http://api.agariohub.io/"
 
 module.exports = {
+	createData: function(json) {
+        return encodeURIComponent(btoa(JSON.stringify(json)).split("/").join("%"));
+    },
+	randomInt: function(min, max) {
+        if (min == max) {
+			return min;
+		}
+        return min + Math.floor(Math.random() * (max - min + 1));
+    },
+	generateUdi: function(size) {
+        var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+          , answer = "";
+        for (var i = 0; i < size; i++)
+            answer += chars[this.randomInt(0, chars.length - 1)];
+        return answer;
+    },
+	theCode: function() {
+        return this.randomInt(1e4, 4e7);
+    },
 	connectLogin: function (page, jsonSend, callback, debug) {
-		var dataSend = encodeURIComponent(btoa(JSON.stringify(jsonSend)).split("/").join("%"))
+		var dataSend = this.createData(jsonSend)
 		if (!dataSend) {
 			console.error("Failed building data when trying to connect to: " + page)
 			return false
