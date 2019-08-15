2e43ccededf9679b55c9cf15c6787e3256d350e3
JJimenez_15 <34722775+JJimenez15@users.noreply.github.com>
2019-08-15 10:12:49 -0600

    Add debugger on api.js

--- api.js
+++ api.js
@@ -7,15 +7,17 @@ function createData(json) {
 	return encodeURIComponent(btoa(JSON.stringify(json)).split("/").join("%"))
 }
 
-function connectLogin(page, jsonSend, callback) {
+function connectLogin(page, jsonSend, callback, debug) {
 	var dataSend = createData(jsonSend)
 	if (!dataSend) {
         console.error("Failed building data when trying to connect to: " + page)
         return false
     }
 	request(`${BASE_URL}${page}/${dataSend}`, (err, req, data) => {
-		var response = data
-		var responseParse = JSON.parse(response)
+		var responseParse = JSON.parse(data)
+		if (debug) {
+			console.log(`[DEBUGGER] ${data}`)
+		}
 		callback(responseParse)
 	});
 }
